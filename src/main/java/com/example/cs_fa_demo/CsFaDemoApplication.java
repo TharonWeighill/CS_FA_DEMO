e312e7f714d7663242456f47e71a3ffb4326c871
tharonweighill <tharon@fusionauth.io>
2022-04-19 17:20:28 -0600

    Initial commit

---
+++ src/main/java/com/example/cs_fa_demo/CsFaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs_fa_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsFaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsFaDemoApplication.class, args);
+	}
+
+}
